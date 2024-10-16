4cb6c8e93db6917d60b1154bbc4a09def3145edb
Puneet0212 <puneet.patil1999@gmail.com>
2024-10-15 20:29:02 -0700

    Revert "commit"
    This reverts commit 0ab77f31fed3f217ffc75e255df38bac27384385.

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,28 @@
-// import { BrowserRouter } from "react-router-dom"
-// import { About, Contact, Experience, Education, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
-// import Resume from "./components/Resume";
-// const App = () => {
-//   return (
-//     <BrowserRouter>
-//       <div className="relative z-0 bg-primary">
-//         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-//           <Navbar />
-//           {/* <Hero />  */}
-//         </div>
-//         <About />
-//         <Education /> 
-//         <Works />
-//         <Tech />
-//         <Experience />
-//         <Resume />
-//         {/* <Feedbacks /> */}
-//         <div className="relative z-0">
-//           <Contact />
-//           <StarsCanvas />
-//         </div>
-//       </div>
-//     </BrowserRouter>
-//   )
-// }
-
-// export default App
-
-
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Navbar, Home, Members, Projects, Publications } from './components';
-
+import { BrowserRouter } from "react-router-dom"
+import { About, Contact, Experience, Education, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+import Resume from "./components/Resume";
 const App = () => {
   return (
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/members" element={<Members />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/publications" element={<Publications />} />
-        </Routes>
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero /> 
+        </div>
+        <About />
+        <Education /> 
+        <Works />
+        <Tech />
+        <Experience />
+        <Resume />
+        {/* <Feedbacks /> */}
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export default App;
-
+export default App
